7b4c71797c552b291a5562269e75de3f029ccdaa
DaveT123 <sinchin9001@hotmail.com>
2022-11-02 18:34:52 +0800

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A deserunt esse incidunt nesciunt corporis, quo ducimus nam labore sit in omnis sint facere molestias neque nisi quia fugiat quos? Excepturi repellat numquam dolores aspernatur incidunt consectetur, modi distinctio quaerat praesentium? Minus saepe reprehenderit fugit consectetur obcaecati, eum exercitationem distinctio quibusdam at accusamus, in aspernatur soluta molestiae ipsum sint asperiores placeat culpa. Similique eos iusto perferendis natus provident dicta cumque, obcaecati sed explicabo excepturi facilis veritatis! Minima ducimus sit iste molestiae eum dolorem quibusdam. Molestiae error sunt quae, autem illo laudantium cum quis ipsum, dolorum odio eligendi ea quaerat, ex repellat?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis molestias repellendus, libero excepturi provident alias voluptas quis odio cupiditate, fugit laborum et accusantium quas temporibus at consequatur, repudiandae a adipisci explicabo doloremque laudantium corporis sit expedita. Autem, nesciunt? Quod, assumenda suscipit ullam unde, vel sint repudiandae est excepturi magni in necessitatibus. Commodi, fuga? Deserunt saepe possimus, molestiae voluptate optio rem. Reprehenderit nam assumenda animi, quae modi aliquid asperiores ex. Mollitia, odit, similique esse porro ipsa nisi dicta numquam at fugiat quaerat eius labore in dolorem laboriosam sapiente rerum nemo repellat impedit hic harum molestiae rem placeat veritatis aut. Totam, quis?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: { }
+}
+</script>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>List of jobs</h1>
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
+         <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Jobs',
+  data() {
+    return {
+      jobs: [
+        { title: 'Dev 1', id: 1, details: 'John' },
+        { title: 'Dev 2', id: 2, details: 'Peter' },
+        { title: 'Dev 3', id: 3, details: 'Loon' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+    .job h2{
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+  }
+  .job h2:hover {
+    background: #ddd;
+  }
+  .job a{
+    text-decoration: none;
+  }
+</style>
